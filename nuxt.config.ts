87e7c4f71a800a0d7252972a478a90227871de8f
Eric Rodriguez <ericrodriguez@MacBook-Pro-de-Alten.local>
2024-11-25 21:46:26 +0100

    fix favicon github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
         {
           rel: 'icon',
           type: 'image/x-icon',
-          href: '/favicon.ico',
+          href: '/pokemon-cards-blog/favicon.ico'
         },
       ],
     },
