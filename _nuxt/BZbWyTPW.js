793e286a153dbc306cbd496cafafeee4fddd3aad
eriiicrc <50671255+eriiicrc@users.noreply.github.com>
2024-11-26 21:09:38 +0000

    Deploying to gh-pages from @ eriiicrc94/pokemon-cards-blog@ad193ed17b68962cb7f31ee17c005f00e445a449 🚀

---
+++ _nuxt/BZbWyTPW.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./Crq6O6Yg.js";const r={};function c(n,t){return"HOME"}const o=e(r,[["render",c]]);export{o as default};
--- _nuxt/CdJbn9Fe.js
+++
@@ -1,1 +0,0 @@
-const t={title:{t:0,b:{t:2,i:[{t:3}],s:"Bienvenido a Pokémon"}},description:{t:0,b:{t:2,i:[{t:3}],s:"Aprende cómo gradear tus cartas Pokémon profesionalmente."}}};export{t as default};
