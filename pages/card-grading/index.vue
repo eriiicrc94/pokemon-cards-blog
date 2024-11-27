ea09195004b2bb7931b2d8a659bccf213744a6b8
Eric Rodriguez <ericrodriguez@MacBook-Pro-de-Alten.local>
2024-11-27 21:53:01 +0100

    empty

--- pages/card-grading/index.vue
+++ pages/card-grading/index.vue
@@ -4,7 +4,6 @@
       <h1>{{ t('gradingSection.seo.title') }}</h1>
       <p>{{ t('gradingSection.seo.description') }}</p>
     </header>
-    
     <index-guide
       :title="t('gradingSection.completeGuide')"
       :items="indexGuideItems" />
